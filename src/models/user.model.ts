7591241f928f748f722d0f6bee57f80699ddc879
MohitCode17 <mohit.codes17@gmail.com>
2024-06-04 22:32:23 +0530

    create password hash method

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 import { User } from "../types/userTypes";
 
 const userSchema = new mongoose.Schema<User>(
@@ -19,6 +20,14 @@ const userSchema = new mongoose.Schema<User>(
   },
   { timestamps: true }
 );
+
+// HASHED PASSWORD
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  return next();
+});
 
 const User = mongoose.model<User>("User", userSchema);
 
