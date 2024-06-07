3226aa052229dbe324c9e0a50bd4bcde593c09e7
MohitCode17 <mohit.codes17@gmail.com>
2024-06-07 17:17:33 +0530

    add book model and their types

---
+++ src/models/book.model.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import { Book } from "../types/bookTypes";
+
+const bookSchema = new mongoose.Schema<Book>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    coverImage: {
+      type: String,
+      required: true,
+    },
+    file: {
+      type: String,
+      required: true,
+    },
+    genre: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Book = mongoose.model<Book>("Book", bookSchema);
+
+export default Book;
